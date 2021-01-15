0029789ac893faefb06d9a4569bf1fc8fb7edbba
songokunr1 <pawelm18@gmail.com>
2021-01-15 13:40:27 +0100

    adding db to app

--- app.py
+++ app.py
@@ -1,16 +1,40 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from models import User
 
 from forms import *
 app = Flask(__name__)
 app.secret_key = 'it is hard to hack!'
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://jlbogoeijkdgra:f66d70134f07635fe3a7106c186ac24b6042b0beef003dec056247d512d21c2d@ec2-79-125-59-247.eu-west-1.compute.amazonaws.com:5432/d7ekrn4ndm767n'
+#psql link_to_postgres_heroku
+#CREATE TABLE users (
+# id SERIAL PRIMARY KEY,
+# username VARCHAR (25) UNIQUE NOT NULL
+# password TEXT NOT NULL
+# );
+# \dt
+# \d users
+
+db = SQLAlchemy(app)
 
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
     reg_form = RegistrationForm()
     if reg_form.validate_on_submit():
-        return "Great!"
+        username = reg_form.username.data
+        password = reg_form.password.data
+
+        user_object = User.query.filter_by(username=username).first()
+        if user_object:
+            return "Someone else has taken this username!"
+
+        user = User(username=username, password= password)
+        db.session.add(user)
+        db.session.commit()
+        return "inserted into DB!"
     return render_template("index.html", form=reg_form)
 
 
---
+++ models.py
@@ -0,0 +1,26 @@
+from app import db
+
+
+class User(db.Model):
+    """User model"""
+    """ doc
+    from app import db
+    from models import User
+    db.create_all()
+    user_1 = User(username="pawel", password="pawel")
+    db.session.add(user_1)
+    db.session.commit()
+    User.query.all()
+    User.query.filter_by(username="pawel').all() #list of object
+    User.query.filter_by(username="pawel').first() # element (object)
+    user = User.query.filter_by(username="pawel').first()
+    user.id
+    
+    """
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(25), unique=True, nullable=False)
+    password = db.Column(db.String(25))
+
+    def __repr__(self):
+        return f"{self.username}"
